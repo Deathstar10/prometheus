192df42af02785fc7579422505ccccfad0d36e08
Aravind Samala <aravind.samala96@gmail.com>
2023-06-11 16:37:40 +0530

    implemented the add new location and saved it to local storage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,10 +8,18 @@ import {
 } from "@react-google-maps/api";
 import { useMemo, useState } from "react";
 
+interface Location {
+  lat: number;
+  lng: number;
+}
+
 export default function Home() {
   const center = useMemo(() => ({ lat: 17, lng: 78 }), []);
   const [latitude, setLatitude] = useState(center.lat);
   const [longitude, setLongitude] = useState(center.lng);
+  const [isMapVisible, setMapVisibility] = useState(false);
+
+  const [locations, setLocation] = useState<Location[]>([]);
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
@@ -22,28 +30,60 @@ export default function Home() {
     setLongitude(e.latLng?.lng);
   }
 
+  function addLocation() {
+    setLocation([...locations, { lat: latitude, lng: longitude }]);
+
+    // save this location to Local storage
+    localStorage.setItem("locations", JSON.stringify(locations));
+
+    setMapVisibility(false);
+  }
+
   if (!isLoaded) {
     return <p>Loading...</p>;
   }
   return (
     <>
-      <header className="bg-primaryColor text-[#fff] h-20">
-        <h1>Prometheus</h1>
-        <p>You can add, edit and delete new locations</p>
-      </header>
-      <main>
-        <button className="bg-[#fff] text-primaryColor rounded border border-primaryColor w-3/5 py-2 md:w-1/5">
-          Add new Location
-        </button>
-        <GoogleMap
-          zoom={10}
-          center={center}
-          mapContainerClassName="w-full h-screen"
-          onClick={handleClick}
-        >
-          <MarkerF position={{ lat: latitude, lng: longitude }}></MarkerF>
-        </GoogleMap>
-      </main>
+      {isMapVisible ? (
+        <>
+          <header className="bg-primaryColor text-[#fff] h-1/5">
+            <button className="pl-2 pt-2" onClick={addLocation}>
+              Add new Location
+            </button>
+            <p className="pl-2">
+              {latitude} , {longitude}
+            </p>
+          </header>
+          <main>
+            <GoogleMap
+              zoom={10}
+              center={center}
+              mapContainerClassName="w-full h-screen"
+              onClick={handleClick}
+            >
+              <MarkerF position={{ lat: latitude, lng: longitude }}></MarkerF>
+            </GoogleMap>
+          </main>
+        </>
+      ) : (
+        <>
+          <header className="bg-primaryColor text-[#fff] h-20 flex justify-between">
+            <div>
+              <h1>Prometheus</h1>
+              <p>You can add, edit and delete new locations</p>
+            </div>
+            <div></div>
+          </header>
+          <div className="flex justify-center">
+            <button
+              onClick={() => setMapVisibility(true)}
+              className="bg-[#fff] text-primaryColor w-3/5 md:w-1/5 py-2 border rounded border-primaryColor mt-10 mx-auto font-bold"
+            >
+              Add new Locations
+            </button>
+          </div>
+        </>
+      )}
     </>
   );
 }
